bdf670e5a2eef908d975260dfe282de8db7a8792
scruz <jsebas.ct@gmail.com>
2022-08-14 13:17:13 -0400

    stage 2

--- Coffee Machine/task/main.go
+++ Coffee Machine/task/main.go
@@ -3,11 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Starting to make a coffee")
-	fmt.Println("Grinding coffee beans")
-	fmt.Println("Boiling water")
-	fmt.Println("Mixing boiled water with crushed coffee beans")
-	fmt.Println("Pouring coffee into the cup")
-	fmt.Println("Pouring some milk into the cup")
-	fmt.Println("Coffee is ready!")
+	fmt.Println("Write how many cups of coffee you will need:")
+
+	cups := 0
+	fmt.Scan(&cups)
+	fmt.Printf("For %d cups of coffee you will need:\n", cups)
+
+	var (
+		waterUnit         = 200
+		milkUnit          = 50
+		coffeeBeansByUnit = 15
+	)
+
+	fmt.Printf("%d ml of water\n", waterUnit*cups)
+	fmt.Printf("%d ml of milk\n", milkUnit*cups)
+	fmt.Printf("%d g of coffee beans\n", coffeeBeansByUnit*cups)
+
 }
