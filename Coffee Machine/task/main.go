e8820f184052b1b8f69b0ab56cbd8784ac29f496
scruz <jsebas.ct@gmail.com>
2022-09-08 12:09:02 -0400

    stage 4

--- Coffee Machine/task/main.go
+++ Coffee Machine/task/main.go
@@ -1,58 +1,131 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func min3(n1 int, n2 int, n3 int) int {
-	var res = 0
-	if n1 <= n2 && n1 <= n3 {
-		res = n1
-	} else if n2 <= n1 && n2 <= n3 {
-		res = n2
-	} else {
-		res = n3
-	}
-	return res
+type CoffeeMachine struct {
+	waterML        int
+	milkML         int
+	coffeeBeansGR  int
+	disposableCups int
+	money          int
 }
 
+func (machine *CoffeeMachine) GetStatus() string {
+	a := fmt.Sprintf("The coffee machine has:\n%d ml of water\n", machine.waterML)
+	b := fmt.Sprintf("%d ml of milk\n%d g of coffee beans\n", machine.milkML, machine.coffeeBeansGR)
+	c := fmt.Sprintf("%d disposable cups\n$%d of money", machine.disposableCups, machine.money)
+	return fmt.Sprintf("%s%s%s", a, b, c)
+}
+
+func (machine *CoffeeMachine) BuyExpresso() {
+	machine.waterML = machine.waterML - 250
+	machine.coffeeBeansGR = machine.coffeeBeansGR - 16
+	machine.money += 4
+	machine.disposableCups -= 1
+}
+
+func (machine *CoffeeMachine) BuyLatte() {
+	machine.waterML = machine.waterML - 350
+	machine.milkML -= 75
+	machine.coffeeBeansGR = machine.coffeeBeansGR - 20
+	machine.money += 7
+	machine.disposableCups -= 1
+}
+
+func (machine *CoffeeMachine) BuyCappuccino() {
+	machine.waterML = machine.waterML - 200
+	machine.milkML -= 100
+	machine.coffeeBeansGR = machine.coffeeBeansGR - 12
+	machine.money += 6
+	machine.disposableCups -= 1
+}
+
+func (machine *CoffeeMachine) Refill(waterML, milkML, coffeeGR, cups int) {
+	machine.waterML += waterML
+	machine.milkML += milkML
+	machine.coffeeBeansGR += coffeeGR
+	machine.disposableCups += cups
+}
+
+func (machine *CoffeeMachine) TakeMoney() int {
+	var money = machine.money
+	machine.money = 0
+	return money
+}
+
+const (
+	Buy  string = "buy"
+	Fill        = "fill"
+	Take        = "take"
+)
+
+const (
+	Expresso   int = 1
+	Late           = 2
+	Cappuccino     = 3
+)
+
 func main() {
-	var waterAvailableMl = 0
-	fmt.Println("Write how many ml of water the coffee machine has:")
-	fmt.Scan(&waterAvailableMl)
+	machine := CoffeeMachine{
+		waterML:        400,
+		milkML:         540,
+		coffeeBeansGR:  120,
+		disposableCups: 9,
+		money:          550,
+	}
+	fmt.Println(machine.GetStatus())
 
-	var milkAvailableML = 0
-	fmt.Println("Write how many ml of milk the coffee machine has:")
-	fmt.Scan(&milkAvailableML)
+	var action = ""
+	fmt.Println("Write action (buy, fill, take):")
+	fmt.Scan(&action)
 
-	var coffeeAvailableGrams = 0
-	fmt.Println("Write how many grams of coffee beans the coffee machine has:")
-	fmt.Scan(&coffeeAvailableGrams)
+	// TODO check if valid action
+	if action == Buy {
+		buyCoffeeAction(&machine)
+	} else if action == Fill {
+		fillAction(&machine)
+	} else if action == Take {
+		moneyTaken := machine.TakeMoney()
+		fmt.Printf("I have you $%d/n", moneyTaken)
+	}
+	fmt.Println(machine.GetStatus())
+}
+
+func fillAction(machine *CoffeeMachine) {
+	var waterAddedMl = 0
+	fmt.Println("Write how many ml of water you want to add:")
+	fmt.Scan(&waterAddedMl)
+
+	var milkAddedML = 0
+	fmt.Println("Write how many ml of milk you want to add:")
+	fmt.Scan(&milkAddedML)
+
+	var coffeeAddedGrams = 0
+	fmt.Println("Write how many grams of coffee beans you want to add:")
+	fmt.Scan(&coffeeAddedGrams)
 
 	cups := 0
 	fmt.Println("Write how many cups of coffee you will need:")
 	fmt.Scan(&cups)
 
-	var (
-		waterUnit         = 200
-		milkUnit          = 50
-		coffeeBeansByUnit = 15
-	)
+	machine.Refill(waterAddedMl, milkAddedML, coffeeAddedGrams, cups)
+}
 
-	requiredWater := waterUnit * cups
-	requiredMilk := milkUnit * cups
-	requiredCoffe := coffeeBeansByUnit * cups
+func buyCoffeeAction(machine *CoffeeMachine) {
+	fmt.Println("What do you want to buy? 1 - expresso, 2 - latte, 3 - cappucino")
+	var coffeeType = 0
+	fmt.Scan(&coffeeType)
 
-	var maxCupWater = waterAvailableMl / waterUnit
-	var maxCupMilk = milkAvailableML / milkUnit
-	var maxCoffee = coffeeAvailableGrams / coffeeBeansByUnit
-	var minOfMaxs = min3(maxCupWater, maxCupMilk, maxCoffee)
-
-	if waterAvailableMl >= requiredWater && milkAvailableML >= requiredMilk && coffeeAvailableGrams >= requiredCoffe {
-		fmt.Print("Yes, I can make that amount of coffee")
-		if minOfMaxs > cups {
-			fmt.Printf(" (and even %d more than that)\n", minOfMaxs-cups)
-		}
-		fmt.Printf("\n")
+	// evaluate
+	if coffeeType == Expresso {
+		machine.BuyExpresso()
+	} else if coffeeType == Late {
+		machine.BuyLatte()
+	} else if coffeeType == Cappuccino {
+		machine.BuyCappuccino()
 	} else {
-		fmt.Printf("No, I can make only %d cups of coffee\n", minOfMaxs)
+		fmt.Println("unkonw action")
 	}
 }
